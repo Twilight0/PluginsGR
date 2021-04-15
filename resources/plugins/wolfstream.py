8e6406e679657476ad3406e5f1af9805b5fda4c2
Twilight0 <1566256+Twilight0@users.noreply.github.com>
2021-04-16 02:22:05 +0300

    Version 0.0.12

--- resources/plugins/wolfstream.py
+++ resources/plugins/wolfstream.py
@@ -56,3 +56,7 @@ class WolfstreamResolver(ResolveUrl):
 
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, 'https://{host}/embed-{media_id}.html')
+
+    @classmethod
+    def _is_enabled(cls):
+        return True
